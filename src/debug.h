f7df9b21cd23002fc16d9a52a7bf5bfcab9c0fd4
Jack Riches <jack.riches@thisisglobal.com>
2013-04-27 17:59:29 +0100

    Debug macro append new line

--- src/debug.h
+++ src/debug.h
@@ -2,7 +2,7 @@
 
 #ifdef _DEBUG
 //#define DEBUG(msg) std::cerr << #msg << std::endl;
-#define DEBUG(...) fprintf(stderr, __VA_ARGS__);
+#define DEBUG(...) fprintf(stderr, __VA_ARGS__); fprintf(stderr, "\n");
 #else
 #define DEBUG(...)
 #endif
